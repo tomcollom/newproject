8c0b8677a92c82ac54f77a80b8e5a49d1000810b
tomcollom <tomcollom@googlemail.com>
2018-09-17 18:28:46 +0000

    save2

--- app/controllers/wait.rb
+++ app/controllers/wait.rb
@@ -1,28 +1,36 @@
-require 'fileutils'
 require 'selenium-webdriver'
 require 'watir'
+require 'nokogiri'
+
+
+
 
 
 
 options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
-
 driver = Selenium::WebDriver.for(:chrome, options: options)
 
-driver.get('http://stackoverflow.com/')
+driver.get('https://www.timeanddate.com/weather/uk/london/historic')
+
 puts driver.title
+
+#selectbox = driver.find_element(:id, 'wt-his-select')
+#option = Selenium::WebDriver::Support::Select.new(selectbox)
+#option.select_by(:value, '20180903')
+
+# Find select option and click
+driver.find_element(:id, "wt-his-select").find_element(:css,"option[value='20180904']").click
+
+# Wait for Browser to load
+sleep 5
+
+# Aknowledge browser is back
+puts "woken"
+
+puts driver.find_element(:class, "sticky-wr").text
+
 
 driver.quit
 
-#driver = Selenium::WebDriver.for:chrome
-
-#driver.get "https://www.google.com"
-
-#browser = Watir::Browser.new :chrome
-
-#browser.goto('http://stackoverflow.com/')
-
-#puts browser.title
-
-#browser.close
 
 
